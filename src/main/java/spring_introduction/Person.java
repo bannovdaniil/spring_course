98b559ea7c407675c18efbbc3fe282d540a3d6d2
bannovdaniil <baddan@mail.ru>
2022-03-12 18:37:20 +0300

    6. Dependency Injection с помощью конструктора

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -7,6 +7,11 @@ public class Person {
         this.pet = pet;
     }
 
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
+        this.pet = pet;
+    }
+
     public void callYourPet(){
         System.out.println("Hello, my lovely Pet!");
         pet.say();
