ecee740639fe5dc3bb1f948f6b814de797702cc6
bannovdaniil <baddan@mail.ru>
2022-03-12 18:59:47 +0300

    8. Внедрение строк и других значений

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -2,6 +2,8 @@ package spring_introduction;
 
 public class Person {
     private Pet pet;
+    private String sutName;
+    private int age;
 
     public Person() {
         System.out.println("Person is created");
@@ -10,6 +12,22 @@ public class Person {
     public void callYourPet() {
         System.out.println("Hello, my lovely Pet!");
         pet.say();
+    }
+
+    public String getSutName() {
+        return sutName;
+    }
+
+    public void setSutName(String sutName) {
+        this.sutName = sutName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void setPet(Pet pet) {
--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -6,12 +6,12 @@ public class Test3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-//        Pet pet = context.getBean("myPet", Pet.class);
-//        Pet dog = new Cat();
-//        Person person = new Person(pet);
-//        person.callYourPet();
+
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+        System.out.println("SutName = " + person.getSutName());
+        System.out.println("Age = " + person.getAge());
+
         context.close();
     }
 }
